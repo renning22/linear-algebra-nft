333c9301e1fe82db00200ccaee0fd32a6cc87ec6
Ning Ren <renning22@gmail.com>
2022-05-27 04:06:07 -0700

    visualize floodfill result

--- notebook/multiprocess.py
+++ notebook/multiprocess.py
@@ -1,15 +1,20 @@
 
 import itertools
+import random
 from multiprocessing import Pool
-
 
 from matrix import mat_mul_by_idx
 
 pool = Pool(64)
 
+# 1 million matmul a batch
+_MAX_SAMPLES = 1_000_000
+
+
 def one_step_by_idx(s):
-    products = itertools.product(s, s)
-    new_s = pool.map(mat_mul_by_idx, products)
+    products = list(itertools.product(s, s))
+    subset = random.sample(products, k=min(_MAX_SAMPLES, len(products)))
+    new_s = pool.map(mat_mul_by_idx, subset)
     return s.union(new_s)
 
 
